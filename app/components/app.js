ff200b99ca8a4da1e9ae352e5ce34c01411ff8ea
iamvanja <vanja@gavric.org>
2017-03-25 17:50:56 -0700

    Added react and basic app render

---
+++ app/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="app">
+            <h1>Blackjack</h1>
+        </div>
+    );
+}
+
+export default App;
--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
-console.log('foo!');
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+import App from './components/app';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
