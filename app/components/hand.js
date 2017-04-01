f40023e0ccb5d7b701587606bca08b16dd673c17
iamvanja <vanja@gavric.org>
2017-04-01 13:57:20 -0700

    Added dealing animation (not all cards at once)

--- app/components/hand.js
+++ app/components/hand.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Card from './card';
 import CSSTransitionGroup from 'react-addons-css-transition-group';
 
@@ -6,33 +6,60 @@ import CSSTransitionGroup from 'react-addons-css-transition-group';
  * Hand component where dealt cards are rendered.
  *
  * @class      Hand (name)
- * @param      {Object}       props        Component properties
- * @param      {Array}        props.cards  The cards
- * @param      {Integer}      props.score  The score
+ * @param      {Object}       props             Component properties
+ * @param      {Array}        props.cards       The cards
+ * @param      {Integer}      props.score       The score
+ * @param      {Bool}         props.inProgress  Is game in progress
+ * @param      {String}       props.owner       Hand's owner (player or dealer)
  * @return     {ReactElement} markup
  */
-const Hand = ({ cards, score }) => {
-    return (
-        <div className="hand">
-            {score &&
-                <div className="score">
-                    <span className="score-value">{score}</span>
+class Hand extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isDealing: false,
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const nextIsDealing = (nextProps.cards.length <= 2) && nextProps.inProgress;
+        if (this.state.isDealing !== nextIsDealing) {
+            this.setState({
+                isDealing: nextIsDealing,
+            })
+        }
+    }
+
+    render() {
+        const { score, cards, owner } = this.props;
+        const { isDealing } = this.state;
+
+        const dataAttributes = {
+            'data-dealing': isDealing,
+            'data-owner': owner,
+        };
+        return (
+            <div className="hand" {...dataAttributes}>
+                {score &&
+                    <div className="score">
+                        <span className="score-value">{score}</span>
+                    </div>
+                }
+                <div className="cards">
+                    <CSSTransitionGroup transitionName="list" transitionEnterTimeout={200} transitionLeaveTimeout={200}>
+                        {cards.map((card, i) =>
+                            <Card
+                                rank={card.rank}
+                                suit={card.suit}
+                                isFaceDown={card.rank === 'dummy'}
+                                key={i}
+                            />
+                        )}
+                    </CSSTransitionGroup>
                 </div>
-            }
-            <div className="cards">
-                <CSSTransitionGroup transitionName="list" transitionEnterTimeout={200} transitionLeaveTimeout={200}>
-                    {cards.map((card, i) =>
-                        <Card
-                            rank={card.rank}
-                            suit={card.suit}
-                            isFaceDown={card.rank === 'dummy'}
-                            key={i}
-                        />
-                    )}
-                </CSSTransitionGroup>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 /**
@@ -41,6 +68,8 @@ const Hand = ({ cards, score }) => {
 Hand.propTypes = {
     cards: PropTypes.array.isRequired,
     score: PropTypes.number,
+    inProgress: PropTypes.bool.isRequired,
+    owner: PropTypes.string,
 };
 
 export default Hand;
